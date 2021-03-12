f2e249b080b8e5f1880183e63c593c0534f4b1e4
Salavat <garaev.salavat@gmail.com>
2021-03-12 20:51:46 +0300

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { UsersService } from './users.service';
 
-
-export interface Task {
-  name: string;
-  completed: boolean;
-  color: ThemePalette;
-  subtasks?: Task[];
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,17 +9,6 @@ export interface Task {
 })
 export class AppComponent implements OnInit {
   title = 'angular-homework11';
-
-  task: Task = {
-    name: 'Indeterminate',
-    completed: false,
-    color: 'primary',
-    subtasks: [
-      {name: 'Primary', completed: false, color: 'primary'},
-      {name: 'Accent', completed: false, color: 'accent'},
-      {name: 'Warn', completed: false, color: 'warn'}
-    ]
-  };
 
   constructor(public users: UsersService){}
 
