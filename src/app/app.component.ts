b99cb00f8a66b9e831b26ddeef9c0a47e0b998fe
Salavat <garaev.salavat@gmail.com>
2021-03-13 15:44:44 +0300

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,10 +44,11 @@ export class AppComponent implements OnInit {
     user.phone = this.userForm.get('phone').value;
     user.website = this.userForm.get('website').value;
 
-    if (userId === 0) {
+    if (userId === 0 || userId == null) {
       user.id = this.users.users.length + 1;
       this.users.users.push(user);
       this.users.postUser(user);
+      this.userForm.reset();
     } else {
       let userIndex: number = this.users.users.findIndex((o) => o.id === userId)
       this.users.users[userIndex].id = userId;
@@ -59,11 +60,14 @@ export class AppComponent implements OnInit {
       this.users.users[userIndex].website = user.website;
 
       this.users.putUser(user, userIndex);
+      this.userForm.reset();
+
     }
   }
 
-  editUser(userToForm: any, i: number) {
+  editUser(userToForm: any) {
     this.userForm.get('id').setValue(userToForm.id);
+    this.userForm.get('name').setValue(userToForm.name);
     this.userForm.get('username').setValue(userToForm.username);
     this.userForm.get('email').setValue(userToForm.email);
     this.userForm.get('address').setValue(userToForm.address);
