335edb5eeacdd1dc75fad4c8639d20764fbbfe85
Salavat <garaev.salavat@gmail.com>
2021-03-13 15:59:38 +0300

    6

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,14 +1,14 @@
 export class User {
-  id: 11;
-  name: 'Clementina DuBuque';
-  username: 'Moriah.Stanton';
-  email: 'Rey.Padberg@karina.biz';
-  address: '31428-2261, Lebsackbury, Kattie Turnpike, Suite 198';
-  phone: '024-648-3804';
-  website: 'ambrose.net';
+  id: 0;
+  name: '';
+  username: '';
+  email: '';
+  address: '';
+  phone: '';
+  website: '';
   company: {
-    name: 'Hoeger LLC';
-    catchPhrase: 'Centralized empowering task-force';
-    bs: 'target end-to-end models';
+    name: '';
+    catchPhrase: '';
+    bs: '';
   };
 }
