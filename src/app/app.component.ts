c0bb876de6043673b9ddb721c1a8d78e25345152
Salavat <garaev.salavat@gmail.com>
2021-03-17 08:06:08 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatTableDataSource } from '@angular/material/table';
-import { timeout } from 'rxjs/operators';
-import { Address, User } from './user';
-import { UsersService } from './users.service';
+import {AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatTableDataSource} from '@angular/material/table';
+import {UsersService} from './users.service';
+import {User} from './user';
+import {timeout} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewChecked {
   title = 'angular-homework11';
 
-  sendButtonActive: boolean = false;
+  sendButtonActive = false;
 
   displayedColumns: string[] = ['id', 'name', 'email', 'zipcode', 'city', 'street', 'suite'];
 
@@ -21,7 +22,7 @@ export class AppComponent implements OnInit {
 
   userForm: FormGroup;
 
-  constructor(public users: UsersService, fb: FormBuilder, addresfb: FormBuilder) {
+  constructor(public users: UsersService, fb: FormBuilder) {
     users.loadUsers();
 
     this.userForm = fb.group({
@@ -29,22 +30,20 @@ export class AppComponent implements OnInit {
       name: fb.control(null, Validators.required),
       username: fb.control(null),
       email: fb.control(null),
-          zipcode: fb.control(''),
-          city: fb.control(''),
-          street: fb.control(''),
-          suite: fb.control(''),
+      zipcode: fb.control(''),
+      city: fb.control(''),
+      street: fb.control(''),
+      suite: fb.control(''),
       phone: fb.control(null),
       website: fb.control(null),
     });
   }
 
   ngOnInit(): void {
+    const users: User[] = this.users.users;
+    }
 
-
-  }
-
-
-  filterFunction(event: Event) {
+  filterFunction(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
@@ -54,14 +53,14 @@ export class AppComponent implements OnInit {
 
     this.dataSource = new MatTableDataSource(this.users.users);
 
-    let user: User = new User();
-    let userId = this.userForm.get('id').value;
-    
+    const user = new User();
+    const userId = this.userForm.get('id').value;
+
 
     user.name = this.userForm.get('name').value;
     user.username = this.userForm.get('username').value;
     user.email = this.userForm.get('email').value;
-    
+
     user.address.zipcode = this.userForm.get('zipcode').value;
     user.address.city = this.userForm.get('city').value;
     user.address.street = this.userForm.get('street').value;
@@ -70,31 +69,31 @@ export class AppComponent implements OnInit {
     user.phone = this.userForm.get('phone').value;
     user.website = this.userForm.get('website').value;
 
-      if (userId === 0 || userId == null) {
-        user.id = this.users.users.length + 1;
-        this.users.users.push(user);
-        this.users.postUser(user);
-        this.userForm.reset();
-      } else {
-        let userIndex: number = this.users.users.findIndex((o) => o.id === userId)
-        this.users.users[userIndex].id = userId;
-        this.users.users[userIndex].name = user.name;
-        this.users.users[userIndex].username = user.username;
-        this.users.users[userIndex].email = user.email;
-        this.users.users[userIndex].address.zipcode =  user.address.zipcode;
-        this.users.users[userIndex].address.city = user.address.city;
-        this.users.users[userIndex].address.street = user.address.street;
-        this.users.users[userIndex].address.suite = user.address.suite;
+    if (userId === 0 || userId == null) {
+      user.id = this.users.users.length + 1;
+      this.users.users.push(user);
+      this.users.postUser(user);
+      this.userForm.reset();
+    } else {
+      const userIndex: number = this.users.users.findIndex((o) => o.id === userId);
+      this.users.users[userIndex].id = userId;
+      this.users.users[userIndex].name = user.name;
+      this.users.users[userIndex].username = user.username;
+      this.users.users[userIndex].email = user.email;
+      this.users.users[userIndex].address.zipcode = user.address.zipcode;
+      this.users.users[userIndex].address.city = user.address.city;
+      this.users.users[userIndex].address.street = user.address.street;
+      this.users.users[userIndex].address.suite = user.address.suite;
 
-        this.users.users[userIndex].phone = user.phone;
-        this.users.users[userIndex].website = user.website;
-        this.users.putUser(user, userIndex);
-        this.userForm.reset();
-      }
+      this.users.users[userIndex].phone = user.phone;
+      this.users.users[userIndex].website = user.website;
+      this.users.putUser(user, userIndex);
+      this.userForm.reset();
     }
+  }
 
 
-  editUser(userToForm: any) {
+  editUser(userToForm: any): void {
     this.userForm.get('id').setValue(userToForm.id);
     this.userForm.get('name').setValue(userToForm.name);
     this.userForm.get('username').setValue(userToForm.username);
@@ -107,12 +106,19 @@ export class AppComponent implements OnInit {
     this.userForm.get('website').setValue(userToForm.website);
   }
 
-  deleteUser(i: number) {
+  deleteUser(i: number): void {
     this.users.users.splice(i, 1);
     this.users.deleteUser(i);
   }
 
-  resetForm() {
+  resetForm(): void {
     this.userForm.reset();
   }
+
+  ngAfterViewChecked(): void {
+    if (!!(this.users.users)) {
+      console.log('ngAfterViewChecked', this.users.users.length);
+    }
+  }
+
 }
